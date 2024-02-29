f9adb2ee8030fdd2f0e44f74f6c3ca0d450ed978
DawnSovereign <jason.t.kim2@gmail.com>
2024-02-29 10:07:30 -0800

    Updated some physics. Started a score tracker. Added platform for loading them in, clearing previous platforms, and pushing down the platforms.

--- doodlejump.js
+++ doodlejump.js
@@ -12,19 +12,6 @@ let doodlerY = boardHeight*7/8 - doodlerHeight; // Initial Y position of the doo
 let doodlerRightImg; 
 let doodlerLeftImg;
 
-// game physics
-let velocityX = 0; 
-let velocityY = 0; //jump speed
-let initialVelocityY = -8; //starting velocity Y
-let gravity = 0.2;
-
-
-//platforms
-let platformArray = [];
-let platformWidth = 60;
-let platformHeight = 18;
-let platformImg;
-
 // Define doodler object with its properties
 let doodler = {
     img : null, // Image object for the doodler
@@ -33,6 +20,23 @@ let doodler = {
     width : doodlerWidth, 
     height : doodlerHeight 
 }
+
+
+// game physics
+let velocityX = 0; 
+let velocityY = -2; //jump speed
+let initialVelocityY = -5.5; //starting velocity Y
+let gravity = 0.07;
+
+
+//platforms
+let platformArray = [];
+let platformWidth = 60;
+let platformHeight = 18;
+let platformImg;
+
+let score = 0;
+
 
 // Function to execute when the window loads
 window.onload = function(){
@@ -91,25 +95,42 @@ function update(){
     // Draw the doodler on the canvas
     context.drawImage(doodler.img, doodler.x, doodler.y, doodler.width, doodler.height);
 
+    //clear platforms
+    while(platformArray.length > 0 && platformArray[0].y >= boardHeight){
+        platformArray.shift(); //removes first element in array 
+        newPlatform();
+    }
+
+
     //platforms
     for(i = 0; i < platformArray.length; i++){
         let platform = platformArray[i];
+        if(velocityY < 0 && doodler.y < boardHeight*3/4){
+            platform.y -= initialVelocityY; //slide platforms down
+        }
         if(detectCollision(doodler, platform ) && velocityY >= 0){
             velocityY = initialVelocityY; //jump off platform
         }
 
         context.drawImage(platform.img, platform.x, platform.y, platform.width, platform.height);
     }
+
+        //score
+        updateScore();
+        context.fillStyle = "black";
+        context.font = "16px sans-serif";
+        context.fillText(score, 5, 20);
+
 }
 
 // handle doodler movement based on keyboard input
 function moveDoodler(e){
     if(e.code == "ArrowRight" || e.code == "KeyD"){ 
-        velocityX = 4; 
+        velocityX = 1.5; 
         doodler.img = doodlerRightImg; 
     } 
     else if(e.code == "ArrowLeft" || e.code == "KeyA"){ 
-        velocityX = -4; 
+        velocityX = -1.5; 
         doodler.img = doodlerLeftImg; 
     }
 }
@@ -129,15 +150,42 @@ function placePlatforms(){
 
     platformArray.push(platform);
 
-    platform = {
-        img : platformImg,
-        x : boardWidth/2,
-        y : boardHeight - 150,
-        width : platformWidth,
-        height : platformHeight     
-    }
+    // platform = {
+    //     img : platformImg,
+    //     x : boardWidth/2,
+    //     y : boardHeight - 150,
+    //     width : platformWidth,
+    //     height : platformHeight     
+    // }
 
-    platformArray.push(platform);
+    // platformArray.push(platform);
+
+    for(let i = 0; i < 6; i++){
+        let randomX = Math.floor(Math.random() * boardWidth*3/4); //(0-1) * boardWidth*3/4
+        let platform = {
+            img : platformImg,
+            x : randomX,
+            y : boardHeight - 75*i - 150,
+            width : platformWidth,
+            height : platformHeight     
+        }
+    
+        platformArray.push(platform);
+    }
+}
+
+//preloads the next set of platforms
+function newPlatform(){
+    let randomX = Math.floor(Math.random() * boardWidth*3/4); //(0-1) * boardWidth*3/4
+        let platform = {
+            img : platformImg,
+            x : randomX,
+            y : -platformHeight,
+            width : platformWidth,
+            height : platformHeight     
+        }
+    
+        platformArray.push(platform);
 }
 
 function detectCollision(a, b){
@@ -145,4 +193,9 @@ function detectCollision(a, b){
            a.x + a.width > b.x && //a's top right corner passes b's top left corner
            a.y < b.y + b.height && //a's top left corner doesnt reach b's bottom left corner
            a.y + a.height > b.y; //a's bottom left corner doesnt reach b's top left corner
+}
+
+function updateScore(){
+    let points = Math.floor(50*Math.random());//(0-1)*50 --> (0-50)
+    score += points;
 }
